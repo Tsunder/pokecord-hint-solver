0ea177f06a90aafa398e8b2054980366803dbc28
Tsunder <Tsunder@users.noreply.github.com>
2021-08-16 13:22:31 -0400

    INITIAL COMMIT

---
+++ bot.js
@@ -0,0 +1,65 @@
+//
+// todo:
+// slash commands? or mentions
+// - help - list commands and their descriptions
+// - invite - DM the user with an invite? or post as a response
+// - catchfix - sets the server's catchfix
+// light database to store catchfixes per server
+
+const {token, HOMEGUILD, HOMECATCHFIX, PREFIX, HINTSTART, POKETWO_ID, DEBUG} = require("./config.json");
+const {POKEMONLIST} = require("./pokemon.json")
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
+
+client.on('messageCreate', async message => {
+	if(message.content.startsWith(`${PREFIX}ping`))	{
+		message.channel.send(`${client.ws.ping}ms.`);
+		console.log(`pinged! ${client.ws.ping}ms`);
+	}
+
+	if ( message.author.id == POKETWO_ID || DEBUG) {
+		if ( message.content.startsWith(HINTSTART) ) {
+			var text = check(message.content,message.guild.id)
+			message.channel.send(text)
+			}
+		}
+
+
+});
+
+client.once( 'ready', () => { //run getpage on a timed loop, if fail then logirthimically increase duration between checking
+	console.log("poke hint solver bot ready");
+});
+
+function check (text,guildId) {
+	text = text.substring(15,text.length-1)
+	//replacing _ for regex
+	text = text.replace(/(\\_|_)/g,".")
+	var reg = new RegExp(text)
+	var validmons = POKEMONLIST[text.length].filter((mon) => {return mon.match(reg)})
+	if (validmons.length == 0) {
+		if (text.length > 14) {
+			text = text.substr(text.lastIndexOf(" ")+1)
+			text = text.split(":")[0]
+		}
+		reg = new RegExp(text.replace(/\\_/g,"."))
+		validmons = POKEMONLIST[text.length].filter((mon) => {return mon.match(reg)})
+	}
+	
+	if (validmons.length == 0) {
+		return "Sorry! Hint parser failed!"
+	}
+	var joiner = guildId == HOMEGUILD ? `\n${HOMECATCHFIX} `:`\n`;
+	if (validmons.length > 20 ) {
+		var out = validmons.slice(0,10).join(joiner)
+		return `${joiner}${out}\nShowing first 10/${validmons.length} matches.`
+	}
+	else  {
+		var out = validmons.join(joiner)
+		return `${joiner}${out}`
+	}
+	return "Error really badly."
+}
+
+client.login(token)
